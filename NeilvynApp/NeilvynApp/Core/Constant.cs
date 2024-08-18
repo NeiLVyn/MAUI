3e943c2fd629987bf4723f4773737bdcdb2d36aa
Neilvyn <65166115+neilvyn@users.noreply.github.com>
2024-08-18 15:44:24 +0800

    Add constants

--- NeilvynApp/NeilvynApp/Core/Constant.cs
+++ NeilvynApp/NeilvynApp/Core/Constant.cs
@@ -10,6 +10,8 @@ namespace NeilvynApp.Core
     {
         public static string Mapbox_Key = "pk.eyJ1Ijoid2VhdGhlciIsImEiOiJjamxncnYwMmYxYXgxM3BzNWEyNHUzbmhhIn0.UqrZwn6qTa6WJwdHjPVUfA";
         public static string GoogleMap_Key = "AIzaSyBbwrBX3v8RMoiJBvGgQDhP-BuQcHo4eqE";
-        public static string OpenWeather_Key = "1df5c5479bfd6f976ddaedc1ee4043ce";
+        public static string OpenWeatherMap_Key = "cc5db9cf47e272b14285b52bf0e71505";
+        public static string WeatherCom_Key = "d522aa97197fd864d36b418f39ebb323";
     }
 }
+
